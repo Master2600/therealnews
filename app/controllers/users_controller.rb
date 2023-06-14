540ef623f7eb3c36c612f7a42cbedb71d8a00c1a
Juan Escudero <Jotaebe@gmail.com>
2023-06-13 23:42:56 -0400

    version 3 render cambio de variable en controller user en metodo  index de @users  a @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @user = User.all # cambie de @users a @user para ver si ese es el error en render.com
   end
 
   # GET /users/1 or /users/1.json
