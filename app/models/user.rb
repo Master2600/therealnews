3482c3362b83d60343af7168e3f291f304f11338
Juan Escudero <Jotaebe@gmail.com>
2023-06-14 00:18:05 -0400

    version 3 render.com

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :news
+  has_many :comments
+
+  before_create :set_default_role
+
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :age, presence: true
 
   ROLES = {
     guest: 0,
     author: 1,
     admin: 2
   }.freeze
-
-  before_create :set_default_role
-
-  validates :name, presence: true
-  validates :age, presence: true
-
-  has_many :news
-  has_many :comments
 
   def has_role?(role_name)
     role == ROLES[role_name.to_sym]
