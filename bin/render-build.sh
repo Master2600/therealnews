aadcdd544a758ce9335bea9f8110d0853fca3c86
Juan Escudero <Jotaebe@gmail.com>
2023-06-17 20:57:30 -0400

    deploy 6

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,8 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate VERSION=20230609235510
+bundle exec rake db:migrate VERSION=20230610005506
+bundle exec rake db:migrate VERSION=20230610005639
+
 bundle exec rake db:seed
